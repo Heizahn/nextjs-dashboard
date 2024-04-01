e62d26c70a62dac7c6ff55a8c82817f7d5c8d1f9
heizahn <hrbweibezahn@gmail.com>
2024-04-01 01:15:18 -0400

    add functionality to the form create customer

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,57 +6,14 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { signIn } from '@/auth';
 import { AuthError } from 'next-auth';
-
-const FormSchema = z.object({
-  id: z.string(),
-  customerId: z.string({
-    invalid_type_error: 'Por favor seleccione un cliente.',
-  }),
-  amount: z.coerce
-    .number()
-    .gt(0, { message: 'Por favor ingrese un monto como $0.' }),
-  status: z.enum(['pending', 'paid'], {
-    invalid_type_error: 'Por favor seleccione el estado d e la factura',
-  }),
-  date: z.string(),
-});
-
-const FormSchemaCustomer = z.object({
-  id: z.string(),
-  name: z.string({
-    invalid_type_error: 'Por favor ingrese un nombre para el cliente.'
-  }),
-  idCard: z.string({
-    invalid_type_error: 'Por favor ingrese la cedula del cliente.'
-  }),
-  phone: z.string({
-    invalid_type_error: 'Por favor ingrese el numero de telefono.'
-  }),
-  ip: z.string({
-    invalid_type_error: 'Por favor ingrese el numero de telefono.'
-  }),
-  plan: z.coerce
-  .number()
-  .gt(0, { message: 'Por favor ingrese un monto como $0.' }),
-  connectionType: z.string({
-    invalid_type_error: 'Por favor seleccione el tipo de conexion.'
-  }),
-  sector: z.string({
-    invalid_type_error: 'Por favor ingrese el sector.'
-  }),
-  location: z.string({
-    invalid_type_error: 'Por favor ingrese La ubicación.'
-  }),
-  rb: z.string({
-    invalid_type_error: 'Por favor seleccione un Router.'
-  }),
-  box: z.string({
-    invalid_type_error: 'Por favor seleccione una Caja.'
-  }),
-  port: z.string({
-    invalid_type_error: 'Por favor seleccione un Puerto.'
-  })
-})
+// import {
+//   State,
+//   StateCustomerFiber,
+//   StateCustomerWireless,
+//   FormSchema,
+//   FormSchemaCustomerFiber,
+//   FormSchemaCustomerWireless,
+// } from '@/app/lib/schemas';
 
 export type State = {
   errors?: {
@@ -67,8 +24,8 @@ export type State = {
   message: string | null;
 };
 
-export type StateCustomer = {
-  error?: {
+export type StateCustomerFiber = {
+  errors?: {
     name?: string[];
     idCard?: string[];
     phone?: string[];
@@ -80,13 +37,109 @@ export type StateCustomer = {
     rb?: string[];
     box?: string[];
     port?: string[];
-};
+  };
   message: string | null;
 };
 
+export type StateCustomerWireless = {
+  error?: {
+    name?: string[];
+    idCard?: string[];
+    phone?: string[];
+    ip?: string[];
+    plan?: string[];
+    connectionType?: string[];
+    sector?: string[];
+    location?: string[];
+    rb?: string[];
+  };
+  message: string | null;
+};
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string({
+    invalid_type_error: 'Por favor seleccione un cliente.',
+  }),
+  amount: z.coerce.number().gt(0, { message: 'Por favor seleccione el plan' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Por favor seleccione el estado d e la factura',
+  }),
+  date: z.string(),
+});
+
+const FormSchemaCustomerFiber = z.object({
+  id: z.string(),
+  name: z.coerce.string({
+    invalid_type_error: 'Por favor ingrese un nombre para el cliente.',
+  }),
+  idCard: z.string({
+    invalid_type_error: 'Por favor ingrese la cedula del cliente.',
+  }),
+  phone: z.string({
+    invalid_type_error: 'Por favor ingrese el numero de telefono.',
+  }),
+  ip: z.string({
+    invalid_type_error: 'Por favor ingrese el numero de telefono.',
+  }),
+  plan: z.coerce
+    .number()
+    .gt(0, { message: 'Por favor ingrese un monto como $0.' }),
+  connectionType: z.string({
+    invalid_type_error: 'Por favor seleccione el tipo de conexion.',
+  }),
+  sector: z.string({
+    invalid_type_error: 'Por favor ingrese el sector.',
+  }),
+  location: z.string({
+    invalid_type_error: 'Por favor ingrese La ubicación.',
+  }),
+  rb: z.string({
+    invalid_type_error: 'Por favor seleccione un Router.',
+  }),
+  box: z.string({
+    invalid_type_error: 'Por favor seleccione la caja.',
+  }),
+  port: z.string({
+    invalid_type_error: 'Por favor seleccione el puerto.',
+  }),
+});
+
+const FormSchemaCustomerWireless = z.object({
+  id: z.string(),
+  name: z.string({
+    invalid_type_error: 'Por favor ingrese un nombre para el cliente.',
+  }),
+  idCard: z.string({
+    invalid_type_error: 'Por favor ingrese la cedula del cliente.',
+  }),
+  phone: z.string({
+    invalid_type_error: 'Por favor ingrese el numero de telefono.',
+  }),
+  ip: z.string({
+    invalid_type_error: 'Por favor ingrese el numero de telefono.',
+  }),
+  plan: z.coerce
+    .number()
+    .gt(0, { message: 'Por favor ingrese un monto como $0.' }),
+  connectionType: z.string({
+    invalid_type_error: 'Por favor seleccione el tipo de conexion.',
+  }),
+  sector: z.string({
+    invalid_type_error: 'Por favor ingrese el sector.',
+  }),
+  location: z.string({
+    invalid_type_error: 'Por favor ingrese La ubicación.',
+  }),
+  rb: z.string({
+    invalid_type_error: 'Por favor seleccione un Router.',
+  }),
+});
+
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
-const CreateCustomer = FormSchemaCustomer.omit({id: true});
+const CustomerWireless = FormSchemaCustomerWireless.omit({ id: true });
+const CustomerFiber = FormSchemaCustomerFiber.omit({ id: true });
 
 export async function createInvoice(prevState: State, formData: FormData) {
   const validatedFields = CreateInvoice.safeParse({
@@ -188,8 +241,11 @@ export async function authenticate(
   }
 }
 
-export async function createCustomer(prevState: StateCustomer, formData: FormData){
-   const validatedFields = CreateCustomer.safeParse({
+export async function CreateCustomerWireless(
+  prevState: StateCustomerWireless,
+  formData: FormData,
+) {
+  const validatedFields = CustomerWireless.safeParse({
     name: formData.get('name'),
     idCard: formData.get('idCard'),
     phone: formData.get('phone'),
@@ -198,47 +254,95 @@ export async function createCustomer(prevState: StateCustomer, formData: FormDat
     connectionType: formData.get('connectionType'),
     sector: formData.get('sector'),
     location: formData.get('location'),
-    rb: formData.get('rb')
-   })
+    rb: formData.get('rb'),
+  });
 
-   if(!validatedFields.success){
+  if (!(validatedFields.success)) {
     return {
       error: validatedFields.error.flatten().fieldErrors,
-      message: 'Campos faltantes. No se pudo crear el cliente.'
-    }
-   }
+      message: 'Campos faltantes. No se pudo crear el cliente.',
+    };
+  }
 
-   console.log(validatedFields.data)
+  const {
+    name,
+    connectionType,
+    idCard,
+    phone,
+    ip,
+    plan,
+    sector,
+    location,
+    rb,
+  } = validatedFields.data;
 
-   const {name, idCard, phone, ip, plan, connectionType, sector, location, rb} = validatedFields.data
-
-   const validatedBoxPort = CreateCustomer.safeParse({
-    box: formData.get('box'),
-    port: formData.get('port')
-   })
-   const planInCent = plan * 100
-
-   try {
-
-    if(validatedBoxPort.success){
-
-      const {box, port} = validatedBoxPort.data
-      await sql`
-      INSERT INTO customers (name, idCard, phone, ip, plan, connectionType, sector, location, rb, box, port)
-      VALUES (${name}, ${idCard}, ${phone}, ${ip}, ${planInCent}, ${connectionType}, ${sector}, ${location}, ${rb}, ${box}, ${port})
+  const planInCent = plan * 100;
+  try {
+    await sql`
+    INSERT INTO customers (name, idCard, phone, ip, plan, connectionType, sector, location, rb)
+    VALUES (${name}, ${idCard}, ${phone}, ${ip}, ${planInCent}, ${connectionType}, ${sector}, ${location}, ${rb})
     `;
-    } else {
-      await sql`
-        INSERT INTO customers (name, idCard, phone, ip, plan, connectionType, sector, location, rb)
-        VALUES (${name}, ${idCard}, ${phone}, ${ip}, ${planInCent}, ${connectionType}, ${sector}, ${location}, ${rb})
-      `;
-    }
-   } catch(err){
+  } catch (error) {
     return {
       message: 'Database Error: Failed to Create Customer.',
     };
-   }
+  }
 
-   revalidatePath('/dashboard/customers');
-   redirect('/dashboard/customers')
+  revalidatePath('/dashboard/customers');
+  redirect('/dashboard/customers');
+}
+
+export async function CreateCustomerFiber(
+  prevState: StateCustomerFiber,
+  formData: FormData,
+) {
+  const validatedFields = CustomerFiber.safeParse({
+    name: formData.get('name'),
+    idCard: formData.get('idCard'),
+    phone: formData.get('phone'),
+    ip: formData.get('ip'),
+    plan: formData.get('plan'),
+    connectionType: formData.get('connectionType'),
+    sector: formData.get('sector'),
+    location: formData.get('location'),
+    rb: formData.get('rb'),
+    box: formData.get('box'),
+    port: formData.get('port'),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Campos faltantes. No se pudo crear el cliente.',
+    };
+  }
+  const {
+    name,
+    connectionType,
+    idCard,
+    phone,
+    ip,
+    plan,
+    sector,
+    location,
+    rb,
+    box,
+    port,
+  } = validatedFields.data;
+
+  const planInCent = plan * 100;
+
+  try {
+    await sql`
+    INSERT INTO customers (name, idCard, phone, ip, plan, connectionType, sector, location, rb, box, port)
+    VALUES (${name}, ${idCard}, ${phone}, ${ip}, ${planInCent}, ${connectionType}, ${sector}, ${location}, ${rb}, ${box}, ${port})
+    `;
+  } catch (error) {
+    return {
+      message: 'Database Error: Failed to Create Customer.',
+    };
+  }
+
+  revalidatePath('/dashboard/customers');
+  redirect('/dashboard/customers');
 }
--- app/ui/customers/create-client.tsx
+++ app/ui/customers/create-client.tsx
@@ -2,18 +2,18 @@
 
 import { useState } from 'react';
 import { useFormState } from 'react-dom';
-import { createCustomer } from '@/app/lib/actions';
+import { CreateCustomerWireless, CreateCustomerFiber } from '@/app/lib/actions';
 import { Button } from '@/app/ui/button';
 import Link from 'next/link';
 
-
-
 const CreateCustomer = () => {
-  
-  const initialState = {message: null, error: {}}
-  const [state, dispatch] = useFormState(createCustomer, initialState)
+  const [tipoConexion, setTipoConexion] = useState('');
+  const initialState = { message: null, errors: {} };
 
-  const [tipoConexion, setTipoConexion] = useState('antena');
+
+  const [state, dispatch] = useFormState(CreateCustomerWireless, initialState);
+
+  console.log(state);
   return (
     <form action={dispatch} className="grid grid-cols-1 gap-4 p-4">
       <div className="grid grid-cols-2 gap-4">
@@ -24,7 +24,17 @@ const CreateCustomer = () => {
             id="name"
             name="name"
             className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+            defaultValue={''}
+            aria-describedby="customer-error"
           />
+          <div id="customer-error" aria-live="polite" aria-atomic="true">
+            {state.error?.name &&
+              state.error.name.map((err) => (
+                <p className="mt-2 text-sm text-red-500" key={err}>
+                  {err}
+                </p>
+              ))}
+          </div>
         </div>
 
         <div>
@@ -63,15 +73,20 @@ const CreateCustomer = () => {
             id="plan"
             name="plan"
             className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+            defaultValue=""
+            aria-describedby="customer-error"
+
           >
             {tipoConexion === 'fibra' ? (
               <>
                 {' '}
+                <option value="">Selecciona</option>
                 <option value="25">$25 - 60Mb</option>
                 <option value="30">$30 - 100Mb</option>{' '}
               </>
             ) : (
               <>
+                <option value="">Selecciona</option>
                 <option value="20">$20 - 8Mb</option>
                 <option value="25">$25 - 12Mb</option>
                 <option value="30">$30 - 20Mb</option>
@@ -87,9 +102,11 @@ const CreateCustomer = () => {
             name="connectionType"
             className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
             onChange={(e) => setTipoConexion(e.target.value)}
+            defaultValue=""
           >
+            <option value="">Selecciona</option>
             <option value="fibra">Fibra</option>
-            <option value="antena" selected={true}>Antena</option>
+            <option value="antena">Antena</option>
           </select>
         </div>
 
@@ -110,6 +127,7 @@ const CreateCustomer = () => {
             id="location"
             name="location"
             className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+            defaultValue={''}
           />
         </div>
 
@@ -119,15 +137,18 @@ const CreateCustomer = () => {
             id="rb"
             name="rb"
             className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+            defaultValue={''}
           >
             {tipoConexion === 'fibra' ? (
               <>
                 {' '}
-                <option value="0.0.0.0" selected={true}>4011C</option>
+                <option value="">Selecciona</option>
+                <option value="0.0.0.0">4011C</option>
               </>
             ) : (
               <>
-                <option value="192.168.217.1" selected={true}>RB705LV</option>
+                <option value="">Selecciona</option>
+                <option value="192.168.217.1">RB705LV</option>
               </>
             )}
           </select>
@@ -140,7 +161,9 @@ const CreateCustomer = () => {
                 id="box"
                 name="box"
                 className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+                defaultValue={''}
               >
+                <option value="">Selecciona</option>
                 <option value="1T">1T</option>
                 <option value="2T">2T</option>
                 <option value="3T">3T</option>
@@ -149,7 +172,6 @@ const CreateCustomer = () => {
                 <option value="6T">6T</option>
                 <option value="7T">7T</option>
                 <option value="8T">8T</option>
-
                 <option value="1V">1V</option>
                 <option value="2V">2V</option>
                 <option value="3V">3V</option>
@@ -167,6 +189,7 @@ const CreateCustomer = () => {
                 name="port"
                 className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
               >
+                <option value="">Selecciona</option>
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -180,15 +203,26 @@ const CreateCustomer = () => {
           </>
         )}
       </div>
+
       <div className="mt-6 flex justify-end gap-4">
-      <Link
+        <Link
           href="/dashboard/customers"
           className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
         >
           Cancelar
         </Link>
-      <Button type="submit">Crear Cliente</Button>
+        <Button type="submit">Crear Cliente</Button>
       </div>
+
+      {
+        <div id="customer-error" aria-live="polite" aria-atomic="true">
+          {state.message && (
+            <p className="mt-2 text-sm text-red-500" key={state.message}>
+              {state.message}
+            </p>
+          )}
+        </div>
+      }
     </form>
   );
 };
